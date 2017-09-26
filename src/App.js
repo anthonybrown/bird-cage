064681207249d0212c1278d9d5afb0ca81fead67
Tony Brown <tony@tonybrown.me>
2017-09-26 06:45:22 -0400

    initail commit, built small app using MobX to see how it works

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import {inject, observer} from 'mobx-react';
+
+@inject('BirdStore')
+@observer
+class App extends Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const bird = this.bird.value;
+    this.props.BirdStore.addBird(bird);
+    this.bird.value = '';
+  }
+
+  render() {
+    const {BirdStore} = this.props;
+    // same as
+    // const BirdStore = this.props.BirdStore;
+    return (
+      <div className="App">
+        <h2>You have {BirdStore.birdCount} birds.</h2>
+
+        <form onSubmit={e => this.handleSubmit(e)}>
+          <input
+            type='text'
+            placeholder='Enter bird'
+            ref={input => this.bird = input}
+          />
+          <button>Add Bird</button>
+        </form>
+        <ul>
+          {BirdStore.birds.map(bird => (
+            <li key={bird}>
+              {bird}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
